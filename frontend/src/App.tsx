75873372344b5394f7d9c819052969edd90d1b8d
DebdipWritesCode <debdipmukherjee52@gmail.com>
2025-05-20 17:55:55 +0530

    add about section

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,13 @@
+import About from "./components/About"
 
 const App = () => {
   return (
-    <div className="">App</div>
+    <div className=" flex flex-col items-center h-screen bg-gray-100">
+      <h1 className=" my-6 text-orange-400 font-semibold text-5xl font-heading">LeetBud</h1>
+      <div className="w-full p-5">
+        <About />
+      </div>
+    </div>
   )
 }
 
--- frontend/src/components/About.tsx
+++ frontend/src/components/About.tsx
@@ -1,7 +1,11 @@
 
 const About = () => {
   return (
-    <div>About</div>
+    <div className="font-body flex flex-col gap-5 justify-center h-[600px] w-[600px] pl-8">
+      <h2 className="text-6xl font-black text-orange-400">Code Smarter, Not Harder</h2>
+      <h4 className="text-2xl font-semibold text-yellow-500">Your AI-powered coding companion for test cases, insights, and instant results.</h4>
+      <img src={"./bot.webp"} alt="LeetBud" className=" w-[300px]" />
+    </div>
   )
 }
 
